28aa5e25d2c306242ed591a32d849099ecaae916
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-08 19:02:00 -0500

    setting up very simple collection view layout

--- CollectionViewBeginning/ViewController.swift
+++ CollectionViewBeginning/ViewController.swift
@@ -1,20 +1,34 @@
-//
-//  ViewController.swift
-//  CollectionViewBeginning
-//
-//  Created by user139405 on 1/8/19.
-//  Copyright © 2019 kabishau. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // what actually gives you word "private"
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let arrayOfnumber = ["1", "2", "3", "4", "5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = (view.frame.size.width - 10 * 2) / 3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
     }
+}
 
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrayOfnumber.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = arrayOfnumber[indexPath.row]
+        }
+        return cell
+    }
+    
+    
 }
 
